fcaa81db6b2f040b035ac434711a9951f43a60ee
chaos <chaos@chaosdeMacBook-Air.local>
2023-06-27 13:53:27 +0800

    First Step

---
+++ src/main/java/top/xijinian/leetcode100/longest_substring_without_repeating_characters/Solution.java
@@ -0,0 +1,36 @@
+package top.xijinian.leetcode100.longest_substring_without_repeating_characters;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null) {
+            return 0;
+        }
+        int[] countArray = new int[128];
+        char[] charArray = s.toCharArray();
+        int begin = 0;
+        int end = 0;
+        int max = 1;
+        countArray[charArray[end]]++;
+        while (end < charArray.length - 1) {
+            end++;
+            countArray[charArray[end]]++;
+            if (countArray[charArray[end]] == 2) {
+                while (true) {
+                    countArray[charArray[begin]]--;
+                    if (charArray[begin] == charArray[end]) {
+                        begin++;
+                        break;
+                    } else {
+                        begin++;
+                    }
+                }
+            }
+            max = Math.max(max, (end - begin + 1));
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().lengthOfLongestSubstring("abcabcbb"));
+    }
+}
